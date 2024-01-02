5c707e43fcef218c71823ed90864c41947c639d9
Elie <62940355+Matellie@users.noreply.github.com>
2024-01-02 21:46:49 +0100

    App working

--- DigitClassifier_App/app.py
+++ DigitClassifier_App/app.py
@@ -2,21 +2,26 @@ from models import *
 
 from tkinter import *
 from PIL import ImageGrab, Image, ImageTk
+import torch
+import numpy as np
 
 def main():
 
     def draw(event):
+        # Draw on canvas
         x, y = event.x, event.y
         if canvas.old_coords:
             x1, y1 = canvas.old_coords
             canvas.create_line(x, y, x1, y1, width=50, fill='white')
         canvas.old_coords = x, y
 
+        # Get image and display it
         image_8x8px, image_display = get_images()
         display_image(image_display)
 
-        #predict(image_8x8px)
-        #display_prediction()
+        # Predict digit and display it
+        prediction = predict(image_8x8px)
+        display_prediction(prediction)
 
     def reset_coords(event):
         canvas.old_coords = None
@@ -38,27 +43,36 @@ def main():
         img_panel.configure(image=image)
         img_panel.image = image
 
-    def save_image(image):
-        image.save('image.png')
-
     def predict(image):
-        pass
+        pred = None
+        with torch.no_grad():
+            image = np.array(image).astype(np.float32)[:,:,0]
+            image = image.flatten()
+            image = torch.from_numpy(image)
+
+            pred = model(image)
+            pred = torch.argmax(pred).item()
+        
+        return pred
     
     def display_prediction(pred):
-        pass
+        pred_area.configure(text=str(pred))
 
+    # Main window
     app = Tk()
-    app.geometry("800x500")
+    app.geometry("800x550")
     app.title("Digit Detector")
     app.configure(bg='white')
 
+    # App title
     app_title = Label(app, text="Digit Detector\n", font=("Arial", 20), bg='white')
     app_title.pack()
 
+    # Left frame (draw area and clear button)
     left_frame = Frame(app, bg='white')
     left_frame.pack(side=LEFT)
 
-    draw_area_title = Label(left_frame, text="⬇ Draw a digit below ⬇", font=("Arial", 15), bg='white')
+    draw_area_title = Label(left_frame, text="Draw a digit below", font=("Arial", 15), bg='white')
     draw_area_title.pack()
 
     canvas = Canvas(left_frame, width=400, height=400)
@@ -66,6 +80,10 @@ def main():
     canvas.pack()
     canvas.old_coords = None
 
+    clear_button = Button(left_frame, text="Clear", font=("Arial", 15), width=20, height=30, command=lambda: canvas.delete("all"))
+    clear_button.pack()
+
+    # Right frame (image 8x8px and model prediction)
     right_frame = Frame(app, bg='white')
     right_frame.pack()
 
@@ -77,7 +95,7 @@ def main():
     img_panel.image = img
     img_panel.pack()
 
-    pred_area_title = Label(right_frame, text="\n⬇ Prediction ⬇", font=("Arial", 15), bg='white')
+    pred_area_title = Label(right_frame, text="\nPrediction", font=("Arial", 15), bg='white')
     pred_area_title.pack()
 
     pred_area = Label(right_frame, text="None", font=("Arial", 20), bg='white')
@@ -86,11 +104,11 @@ def main():
     model = SimpleNeuralNet(input_size=64, hidden_size=32, nb_classes=10)
     model = load_model(model=model, model_path='model_ckpt', model_name='model_MNISTDigits_SimpleNeuralNet_97.pt')
 
-    
-
+    # Bindings
     app.bind('<B1-Motion>', draw)
     app.bind('<ButtonRelease-1>', reset_coords)
 
+    # Main loop
     app.mainloop()
 
 if __name__ == "__main__":
